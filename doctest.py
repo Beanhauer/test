cec72f1b70756725a796fcada61cefa01c81c3a9
Ann Bieneman <shopann@gmail.com>
2018-08-05 14:15:22 -0400

    create program read document, copy to other document

---
+++ doctest.py
@@ -0,0 +1,13 @@
+def main():
+    import docx
+    doc1 = docx.Document("patent patent Alice.docx")
+    doc2 = docx.Document()
+    fulltext = []
+    for para in doc1.paragraphs:
+        fulltext.append(para.text)
+    print(fulltext)
+    for para in fulltext:
+        doc2.add_paragraph(para)
+        doc2.save("doc2.docx")
+
+main()
